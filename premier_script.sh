3c3ba26b26c85fc935100f6bf63c97243d03fcb3
Mikhail Biriuchinskii <115531770+MichaBiriuchinskii@users.noreply.github.com>
2022-10-12 10:49:38 +0200

    Update premier_script.sh

--- premier_script.sh
+++ premier_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# tratement du texte
+# traitement de texte pendant le Cours 12 Octobre
 echo 'pour l'année 20!6' > sortie.txt
 grep 'Location' 2016*.ann | wc -l >> sortie.txt
 echo 'pour l'année 2017' >> sortie.txt
