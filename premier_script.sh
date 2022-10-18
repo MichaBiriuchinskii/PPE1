69da2582915ceb55aea5e48afc85ac56788690be
Michel <Michel@MacBook-Air-mak.local>
2022-10-18 11:03:16 +0200

    Modifications du premier script

--- premier_script.sh
+++ premier_script.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 # traitement de texte pendant le Cours 12 Octobre
-echo 'pour l'année 20!6' > sortie.txt
+cd /Users/mak/Desktop/TAL Education/Programmation et projet encadré/lecon 2 et 3/PPE/Fichiers
+echo "pour l'année 2016" > sortie.txt
 grep 'Location' 2016*.ann | wc -l >> sortie.txt
-echo 'pour l'année 2017' >> sortie.txt
+echo "pour l'année 2017" >> sortie.txt
 grep 'Location' 2017*.ann | wc -l >> sortie.txt
-echo 'pour l'année 2018' >> sortie.txt
+echo "pour l'année 2018" >> sortie.txt
 grep 'Location' 2018*.ann | wc -l >> sortie.txt
 
 
