e8f75b6bdce0ec9a0d922b6b2b538696954eff89
MichaBiriuchinskii <yosuga723@mail.ru>
2022-12-31 15:32:09 +0100

    modifications

--- script/script_URL_traitement.sh
+++ script/script_URL_traitement.sh
@@ -1,94 +1,90 @@
 #!/usr/bin/env bash
 
-# в начале программы можно добавить условие "этот скрипт должен запускаться в racine и тут должны быть такие-то папки. 
-#Если их нет - писать мол создайте правильную среду для скрипта
+# La condition "ce script doit être exécuté à Racine et il doit y avoir tel ou tel dossier ici" peut être ajoutée au début du programme. 
+#Si ce n'est pas le cas, écrit pour dire qu'il faut créer le bon environnement pour le script.
 
-
-if [ $# -ne 2 ] #si le nombre d'éléments n'est pas égale à 2, on exit, прописываем условия программы
-then 
-		echo "il faut 2 paramètres"
-		echo "Syntaxe : (1) script_URL_traitement.sh (2) nom_fichier_URL (3) nom_fichier_HTML"
-		exit
-fi
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
 
-mot="(имм|эм)игр\w+" 
+#si le nombre d'éléments n'est pas égale à 2, on exit, on précise les conditions du programme
+if [  $# != 2  ]
+then 
+			echo "il faut 2 paramètres"
+			echo "Syntaxe : script_URL_traitement.sh (1) nom_fichier_URL (2) nom_fichier_HTML"
+			exit
+fi
 
-echo $fichier_urls;
+mot=$(egrep "(имм|эм)игр\w+" $fichier_urls)
+
 basename=$(basename -s .txt $fichier_urls)
-#basename -s удаляет путь, оставляя только конечное имя файла. Опция -s удаляет расширение (тж называется suffix)
+#basename -s supprime le chemin, ne laissant que le nom du fichier. L'option -s supprime l'extension (également appelée suffixe)
 
-#здесь начинается создание сайта, прописывается условия для создания таблицы и её оформение
+#c'est ici que l'on commence à créer le site web, en prescrivant les conditions de création du tableau et son design
+
 echo 	"<html>
-	<head>
-		<meta charset=\"utf-8\" /> 
-		<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
-		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-		<title>Tableau des URLS</title>
-	</head>
-	<body>
-		<h1 class=\"title\">Tableau des URLs</h1>
-		<table class=\"table is-bordered is-striped is-narrow is-hoverable is-fullwidth\">
-			<thead><tr><th>code HTTP</th><th>URL</th><th>Encodage</th><th>Aspirations</th><th>Dump</th><th>Count</th><th>Occurences</th><th>Contextes</th><th>Concordances</th></tr></thead>" > $fichier_tableau # направляет сайт, удаляя содержимое предыдущее всего файла
+			<head>
+							<meta charset=\"utf-8\" /> 
+							<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
+							<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+							<title>Tableau des URLS</title>
+			</head>
+			<body>
+							<h1 class=\"title\">Tableau des URLs</h1>
+							<table class=\"table is-bordered is-striped is-narrow is-hoverable is-fullwidth\">
+									<thead><tr><th>ID</th><th>code HTTP</th><th>URL</th><th>Encodage</th><th>HTML</th><th>Dump</th><th>Occurences</th><th>Contextes</th><th>Concordances</th></tr></thead>" > $fichier_tableau # dirige le site en supprimant le contenu du fichier entier précédent
 
-lineno=1; #счетчик линий
-#читаем линии из файла
-#while read -r line;
-#do
-	#URL=$line;
-	#HTTP=$(curl --head $URL | egrep HTTP | cut -d " " -f 2) #здесь определяем переменную, которая ищет начало в доке с урл "ХТТП" и обрезает с помощью пробела текст, берет второй элемент, который нужен (это статус сайта, работает или нет)
-while read -r URL; do
-	echo -e "\tURL : $URL";
-	# ожидаемый способ, без опции -w в cURL
-	code=$(curl -ILs $URL | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | tail -n 1)
-	charset=$(curl -ILs $URL | grep -Eo "charset=(\w|-)+" | cut -d= -f2)
-	charset=$(echo $charset | tr "[a-z]" "[A-Z]") #здесь мы определяем для значения шарсет команду "переделай все маленькие буквы в большие, чтобы всезде UTF-8 были одного вида
-	echo -e "\tcode : $code"; 
+lineno=1; #compteur de lignes
+
+while read -r URL; 
+do
+			echo -e "\tURL : $URL";
+			code=$(curl -ILs $URL | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | tail -n 1) # de la manière attendue, sans l'option -w de cURL
+			charset=$(curl -ILs $URL | grep -Eo "charset=(\w|-)+" | cut -d= -f2)
+			charset=$(echo $charset | tr "[a-z]" "[A-Z]") #ici nous définissons pour la valeur sharset la commande "convertir toutes les petites lettres en majuscules, afin que toutes les lettres UTF-8 soient du même type
+			echo -e "\tcode : $code"; 
+		
+			if [[ -z $charset ]]
+			then
+						echo -e "\tencodage non détecté, on prendra UTF-8 par défaut.";
+						charset="UTF-8";
+			else
+						echo -e "\tencodage : $charset";
+			fi
 	
-	# другой способ, с помощью опции -w в cURL
-	# code=$(curl -Ls -o /dev/null -w "%{http_code}" $URL)
-	# charset=$(curl -ILs -o /dev/null -w "%{content_type}" $URL | grep -Eo "charset=(\w|-)+" | cut -d= -f2)
-	
-	if [[ ! $charset ]]
-	then
-			echo -e "\tencodage non détecté, on prendra UTF-8 par défaut.";
-			charset="UTF-8";
-	else
-			echo -e "\tencodage : $charset";
-	fi
-	
-	if [[ $code -eq 200 ]]
-		aspiration=$(curl $URL > ./aspirations/$basename-$lineno.html)
-	then
-		if [[ $charset == 'UTF-8' ]]
-		then
-			dump=$(curl $URL | lynx -stdin -dump -assume_charset=$charset -display_charset=UTF-8)
-		else
-			dump=$(curl $URL | iconv -f $charset -t UTF-* | lynx -stdin -dump -assume_charset=UTF-8 -display_charset=UTF-8)
-		fi
-	else
-		echo -e "\tcode différent de 200 utilisation d'un dump vide"
-		dump=""
-		charset=""
-	fi
+			if [[  $code -eq 200  ]]
+			then
+						aspiration=$(curl $URL > ./aspirations/$basename-$lineno.html)
+						if [[  $charset == 'UTF-8'  ]]
+						then
+									dump=$(curl $URL | lynx -stdin -dump -assume_charset=$charset -display_charset=UTF-8)
+						else
+									dump=$(curl $URL | iconv -f $charset -t UTF-* | lynx -stdin -dump -assume_charset=UTF-8 -display_charset=UTF-8)
+						fi
+			else
+						echo -e "\tcode différent de 200 utilisation d'un dump vide"
+						dump=""
+						charset=""
+			fi
+			
+			echo "$dump" > "./dumps-text/$basename-$lineno.txt"
+			echo "$aspiration" > "./aspirations/$basename-$lineno.html"
+
 	# compte du nombre d'occurrences
-  NB_OCC=$(grep -E -o $mot ./dumps-text/$basename-$lineno.txt | wc -l)
+	
+NB_OCC=$(grep -E -o $mot ./dumps-text/$basename-$lineno.txt | wc -l)
   # extraction des contextes
-  grep -E -A2 -B2 $mot ./dumps-text/$basename-$lineno.txt > ./contextes/$basename-$lineno.txt
+grep -E -A2 -B2 $mot ./dumps-text/$basename-$lineno.txt > ./contextes/$basename-$lineno.txt
   # construction des concordance avec une commande externe
-  
-  bash programmes/concordance.sh ./dumps-text/$basename-$lineno.txt $mot > ./concordances/$basename-$lineno.html
-	Count=$(echo $dump | egrep -o -i "$mot" | wc -l)
-	echo "$dump" > "./dumps-text/$basename-$lineno.txt"
+bash programmes/concordance.sh ./dumps-text/$basename-$lineno.txt $mot > ./concordances/$basename-$lineno.html
+Count=$(echo $dump | egrep -o -i "$mot" | wc -l)
 	
-	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td><a href=\"../aspirations/$basename-$lineno.html\">html</a></td><td><a href=\"../dumps-text/$basename-$lineno.txt\">text</a></td><td>$NB_OCC</td><td><a href=\"../contextes/$basename-$lineno.txt\">contextes</a></td><td><a href=\"../concordances/$basename-$lineno.html\">concordance</a></td></tr>" >> $fichier_tableau  #тут мы указываем, куда заносить всю инфу и отмечаем, что при каждой итерации не нужно удалять
-	echo -e "\t--------------------------------"
+			echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td><a href=\"../aspirations/$basename-$lineno.html\">html</a></td><td><a href=\"../dumps-text/$basename-$lineno.txt\">text</a></td><td>$NB_OCC</td><td><a href=\"../contextes/$basename-$lineno.txt\">contextes</a></td><td><a href=\"../concordances/$basename-$lineno.html\">concordance</a></td></tr>" >> $fichier_tableau  #Nous spécifions ici où placer toutes les informations et notons qu'il n'est pas nécessaire de les supprimer à chaque itération
+			echo -e "\t--------------------------------"
+			lineno=$((lineno+1));
+			
+done  < $fichier_urls
 
-	lineno=$((lineno+1));
-	done  < $fichier_urls
-echo "</table>" >> $fichier_tableau
-echo "</body></html>" >> $fichier_tableau
+echo "		</table>
+	</body>
+</html>" >> "tableaux/$fichier_tableau"
 	
-# modifier la ligne suivante pour créer effectivement du HTML
-#echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
